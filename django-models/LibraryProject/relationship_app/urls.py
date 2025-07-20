fd548c5995f8609676c4b8c3572bb37544bd6d19
Youssef Shaker <youssefaymanshaker@outlook.com>
2025-07-20 18:38:57 +0300

    organizing urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,19 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import LibraryDetailView, list_books, login_view, logout_view, register_view
+from .views import LibraryDetailView, list_books, register_view
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", login_view.as_view(template_name="relationship_app/login.html"), name="login"),
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
     path(
         "logout/",
-        logout_view.as_view(template_name="relationship_app/logout.html"),
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
         name="logout",
     ),
     path("register/", register_view, name="register"),
