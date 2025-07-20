b52b938b41316f7f82ca970295b7e0991276d039
Youssef Shaker <youssefaymanshaker@outlook.com>
2025-07-20 18:34:16 +0300

    insert template name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import LibraryDetailView, list_books, login_view, logout_view, regis
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", login_view, name="login"),
+    path("login/", login_view.as_view(template_name="relationship_app/login.html"), name="login"),
     path(
         "logout/",
         logout_view.as_view(template_name="relationship_app/logout.html"),
